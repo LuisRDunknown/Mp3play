b441e1681d647fb821b8e0378bc93760bf80a8b7
LuisRangel <lr2315490@gmail.com>
2023-01-18 13:32:13 -0600

    Agregamos lista de caciones

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const songs = [
+    {
+        name: 'song 1',
+        path: 'music/Song 1.mp3',
+        artista: 'artist 1',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'song 2',
+        path: 'music/Song 2.mp3',
+        artista: 'artist 2',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'song 3',
+        path: 'music/Song 3.mp3',
+        artista: 'artist 3',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'song 4',
+        path: 'music/Song 4.mp3',
+        artista: 'artist 4',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'music/Song 5.mp3',
+        artista: 'artist 5',
+        cover: 'images/cover 4.png'
+    },
+]
